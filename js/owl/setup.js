2d3694176522fe9bec51cf7e28c8edf662835a59
Scarceli <diegoscarceli@hotmail.com>
2021-10-25 00:23:58 -0300

    Finalizando projeto RcriandoNetflix

---
+++ js/owl/setup.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{    /*quantidade de item na tela (adapta por pixel)*/
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function carregarPlay(){
+    window.open("https://www.youtube.com/watch?v=a-zOpKW6n2I");
+    
+}
+
+
+
+/*
+https://www.youtube.com/watch?v=a-zOpKW6n2I
+A descoberta do churrasco é atribuída aos índios que habitavam a costa das três Américas. Eles assavam a carne ao ar livre, numa fogueira sobre pedras com auxílio de uma grelha de madeira verde, mas foi na região do grande pampa que o churrasco encontrou o seu ambiente ideal.
+*/
